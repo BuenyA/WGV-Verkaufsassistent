576b1f74fa69ad85d24f0417afff131e09e29187
BuenyA <aydemir.buny@gmail.com>
2024-07-28 02:33:10 +0200

    Testdataset created

--- evaluation/test.py
+++
@@ -1,7 +0,0 @@
-from ragas import evaluate
-from datasets import Dataset
-import os
-
-from langchain_community.document_loaders import DirectoryLoader
-loader = DirectoryLoader("C:\\Users\\aydem\\Documents\\salesassistent\\dataset\\test")
-documents = loader.load()
